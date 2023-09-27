839aa493bb6fbc24a787f55d0542a99000294afe
janine <j.liebscher@bbqberlin.onmicrosoft.com>
2023-09-27 17:50:18 +0200

    logische Operator & Scanner

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+//        logische Operatoren
+        System.out.println("! NICHT");
+        System.out.println("&&  AND");
+        System.out.println("||  OR");
+        System.out.println("^   XOR(nicht beides gleich) ");
+
+//        Scanner
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Gibt eine Zahl ein");
+        int zahl= scan.nextInt();
+        System.out.println(zahl);
+
+        System.out.println("Gibt ein Satz ein");
+        String satz = scan.nextLine();
+        System.out.println(satz);
     }
 }
