da852c65c1f851f122167d5216c934d4043a5996
janine <j.liebscher@bbqberlin.onmicrosoft.com>
2023-10-21 21:10:48 +0200

    Klassen Konstruktor

--- Java/src/KlassenObject1.java
+++ Java/src/KlassenObject1.java
@@ -6,10 +6,35 @@ public class KlassenObject1 {
     String fellfarbe;
     boolean kannPfoteGeben;
 
+    //Konstruktor
+    // brauch kein Rückgabetyp, weiß dadruch das es Kontstruktor ist, weil kein Rückagbetyp da
+    //muss wie Klasse heißen
+    public KlassenObject1(int alter, String art, String fellfarbe, boolean kannPfoteGeben){
+        //aus Klasse   aus Parameter
+        this.alter  = alter;
+        this.art = art;
+        this.fellfarbe = fellfarbe;
+        this.kannPfoteGeben = kannPfoteGeben;
+
+    }
+
+
+
     //Methoden
 
     public static void bellen() {
         System.out.println("bellen");
+    }
+
+    public void testBellen(){
+        bellen(); //kann andere Methoden einfach aufrufen
+    }
+
+    public void kannBellen(boolean kannBellen){
+        if(kannBellen){
+            bellen();
+
+        }
     }
 
     public void rechnen(int zahl1, int zahl2){
--- Java/src/KlassenObjecteMain.java
+++ Java/src/KlassenObjecteMain.java
@@ -2,18 +2,35 @@ public class KlassenObjecteMain {
 
     public static void main(String[] args){
 
+        //################ before Konstruktor ####################
     KlassenObject1.bellen(); // kann darauf zu greifen, weil die Methode in der andern klasse public und static ist
         //muss also nicht extra erste ein obejct davon erzeugt werden, weil static
 
 
-        KlassenObject1 hund = new KlassenObject1(); // erzeugt ein neues Object von Datentyp KlassenObject1
+//        KlassenObject1 hund = new KlassenObject1(); // erzeugt ein neues Object von Datentyp KlassenObject1
 
-        hund.bellen(); //geht aber auch mit neuen Object
+//        hund.bellen(); //geht aber auch mit neuen Object
+//
+//        hund.rechnen(2,4);
+//
+////        KlassenObject1.rechnen(3.2); // geht jetzt nicht ohne erst ein Object zu erstellen,
+//        //da Methode in anderen KLasse nicht static ist
+//
+//        hund.testBellen(); //bellt auch weil die bellen Methoden darin aufgerufen wird
 
-        hund.rechnen(2,4);
+        //########################## after Konstruktor #####################################
 
-//        KlassenObject1.rechnen(3.2); // geht jetzt nicht ohne erst ein Object zu erstellen,
-        //da Methode in anderen KLasse nicht static ist
+        KlassenObject1 labrador = new KlassenObject1(3,"Labdrabdor", "braun", true);
 
+        KlassenObject1 dackel = new KlassenObject1(4,"Dackel", "rot", true);
+
+        System.out.println(labrador.alter);
+
+        KlassenObject1 [] hunde = {labrador, dackel}; //Array mit Objekten
+
+        for (int i = 0; i < hunde.length; i++) {
+            System.out.println(hunde[i].art); //gibt alle Arten von Hunden aus aus array
+
+        }
     }
 }
